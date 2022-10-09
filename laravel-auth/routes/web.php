d04649a15a66cb5d7d27c0e906e91b59675957e5
Stefano Morgia <ste.morgia@gmail.com>
2022-10-09 17:54:18 +0200

    implementazione slide completata

--- laravel-auth/routes/web.php
+++ laravel-auth/routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
@@ -27,3 +25,7 @@ Route::middleware('auth')
             Route::get('/', 'HomeController@index')
             ->name('home');
         });
+
+Route::get("{any?}", function(){
+    return view("guest.home");
+})->where("any",".*");
