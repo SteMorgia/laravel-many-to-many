11e16e0ea385572e8b661f622eb232ebc3f5283f
Stefano Morgia <ste.morgia@gmail.com>
2022-10-12 16:04:06 +0200

    prova push

--- laravel-auth/public/js/front.js
+++ laravel-auth/public/js/front.js
@@ -12622,7 +12622,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\PC\Desktop\Boolean\Laravel\laravel-one-to-many\laravel-auth\laravel-auth\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\PC\Desktop\Boolean\Laravel\laravel-many-to-many\laravel-auth\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
